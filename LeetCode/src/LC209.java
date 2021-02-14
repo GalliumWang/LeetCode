fa836e4b99124a8acee7956473361630e2efe0ae
Gallium Wang <galliumwang199@gmail.com>
2021-02-14 16:26:10 +0800

    LC2019 TLE(log(N^2)) edition

--- LeetCode/src/LC209.java
+++ LeetCode/src/LC209.java
@@ -1,3 +1,4 @@
+import sun.jvmstat.monitor.Variability;
 
 public class LC209 {
 
@@ -10,4 +11,73 @@ public class LC209 {
 
 	}
 
+}
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        Boolean solvable=false;
+        int startIdx=0;
+        int endIdx=0;
+        for(var i=0;i<nums.length;i++) {
+        	if(solvable) {
+        		// when end num not included
+        		var solutionLen=endIdx-startIdx;
+        		
+        		// when end num included
+        		int endhold=i+1;
+        		int tmpStartIdx=0;
+        		int tmpEndIdx=0;
+				for(var j=i;i>=0;j--) {
+					var tmpSum=get_sum(nums, j, endhold);
+					if(tmpSum>=target) {
+						tmpStartIdx=j;
+						tmpEndIdx=endhold;
+						break;
+					}
+				}
+				int tmpSolutionLen=tmpEndIdx-tmpStartIdx;
+				
+				// compare
+				if(tmpSolutionLen<=solutionLen) {
+					startIdx=tmpStartIdx;
+					endIdx=tmpEndIdx;
+				}
+				
+				
+				
+        	}
+        	else {	
+        		//sum up from end
+        		int endhold=i+1;
+        		
+        		
+				for(var j=i;i>=0;j--) {
+					var tmpSum=get_sum(nums, j, endhold);
+					if(tmpSum>=target) {
+						solvable=true;
+						startIdx=j;
+						endIdx=endhold;
+						break;
+					}
+				}
+				
+				
+			}
+        }
+        
+        if(!solvable) {
+        	return 0;
+        }
+        else {
+			return endIdx-startIdx;
+		}
+        
+    }
+    private int get_sum(int[] nums,int startIdx,int endIdx) {
+    	int sum=0;
+		for(var i = startIdx;i<endIdx;i++) {
+			sum+=nums[i];
+		}
+		return sum;
+	}
 }
