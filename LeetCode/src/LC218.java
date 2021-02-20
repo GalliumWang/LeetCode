a522d81703e31e2f0fd1ea87cb1c18e0e022d9af
Gallium Wang <galliumwang199@gmail.com>
2021-02-21 01:57:10 +0800

    pass LC218

--- LeetCode/src/LC218.java
+++ LeetCode/src/LC218.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LC218 {
 
@@ -36,37 +38,77 @@ class Solution218 {
         
         rightBorder=lists.stream().max((x, y) -> x.get(1) - y.get(1)).get().get(1);
         
-        for (int i = leftBorder; i <= rightBorder; i++) {
+        
+        List<Integer> point_set=new ArrayList<>();
+        
+        for(var list:lists) {
+        	point_set.add(list.get(0)-1);
+        	point_set.add(list.get(0));
+        	point_set.add(list.get(0)+1);
+        	
+        	point_set.add(list.get(1)-1);
+        	point_set.add(list.get(1));
+        	point_set.add(list.get(1)+1);
+        }
+        
+        
+        // remove duplicates
+        point_set=point_set.stream()
+        	     .distinct()
+        	     .collect(Collectors.toList());
+        
+        Collections.sort(point_set); 
+        
+        
+        point_set.remove(point_set.size()-1);
+        
+        
+        for (int i:point_set) {
         	final int i_copy=i;
         	//get highest value
         	var outValue=0;
 			var outList=lists.stream().max(
 				(x,y) -> {
 					var xMax=0;
-					if(x.get(0)<=i_copy && x.get(1)>=i_copy) xMax=x.get(2);
+					if(x.get(0)<=i_copy && x.get(1)>i_copy) xMax=x.get(2);
 					var yMax=0;
-					if(y.get(0)<=i_copy && y.get(1)>=i_copy) yMax=y.get(2);
+					if(y.get(0)<=i_copy && y.get(1)>i_copy) yMax=y.get(2);
 					return xMax-yMax;
 				}).get();
 			
-			if(outList.get(0)<=i && outList.get(1)>=i) {
+			if(outList.get(0)<=i && outList.get(1)>i) {
 				outValue=outList.get(2);
 			}
 			
+//			System.out.println("=====");
+//			System.out.println(i);
+//			System.out.println(outValue);
+			
 			
 			drawingpoint=outValue;
-			if(drawingpoint!=drawingpointPre) {
+			if(drawingpoint>drawingpointPre) {
 				resList.add(
 						List.of(i,drawingpoint)
 						);
 				
 			}
+			else if (drawingpoint<drawingpointPre) {
+				resList.add(
+						List.of(i,drawingpoint)
+						);
+			}
+			
+			
 								  		
 			drawingpointPre=drawingpoint;
 			
 		}
         
+        //resList.add(List.of(rightBorder,0));
         
+        if(rightBorder==2147483647) {
+        	resList.add(List.of(2147483647,0));
+        }
         
     	
     	return resList;
