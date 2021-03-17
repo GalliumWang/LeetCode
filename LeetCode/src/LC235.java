e0c79f2884bee6686351188f525fc5d7ca035813
Gallium Wang <galliumwang199@gmail.com>
2021-03-17 16:28:33 +0800

    add uncommitted LC235

---
+++ LeetCode/src/LC235.java
@@ -0,0 +1,87 @@
+
+public class LC235 {
+
+	public LC235() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
+
+
+
+
+class Solution235 {
+	public class TreeNode {
+	    int val;
+	    TreeNode left;
+	    TreeNode right;
+	    TreeNode(int x) { val = x; }
+	}
+	
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+     
+    	int a=Math.min(p.val, q.val);
+    	int b=Math.max(p.val, q.val);
+
+    	outLoop:
+    	while(true) {
+    		var tmpInt=check_position(root.val, p.val, q.val);
+    		switch (tmpInt) {
+			case -1:
+				root=root.right;
+				break;
+			case 1:
+				root=root.left;
+				break;
+			case 0:
+				break outLoop;
+			default:
+				break;
+			}
+    		
+    		break;
+    	}
+
+    	return root;
+    }
+
+    private int check_position(int checked_num,int a,int b) {
+		if(checked_num>=a&&checked_num<=b) {
+			return 0;
+		}
+		else if(checked_num<=a) {
+			return -1;
+		}
+		else {
+			return 1;
+		}
+		
+	}
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
