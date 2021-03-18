048b50656e87fb6bf1a5a93c841fc8586353d824
Gallium Wang <galliumwang199@gmail.com>
2021-03-18 13:09:40 +0800

    add space solution descreption for LC238

---
+++ LeetCode/src/LC238T.java
@@ -0,0 +1,43 @@
+
+public class LC238T {
+
+	public LC238T() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
+
+/**
+ * 
+ * use a two dimension array below
+ * 
+ * 0 1 . . . 1
+ * 1 0 . . . 1
+ * 1 1 . . . 1
+ * . . . . . . 
+ * 1 1 . . . 0
+ * 
+ * [the array above is just for concept and can be avoid in code]
+ * 
+ * to multiply the res array of [1 1 . . . . 1]
+ *
+ */
+
+class Solution238_space {
+    public int[] productExceptSelf(int[] nums) {
+        
+    	
+    	return null;
+    }
+}
+
+
+
+
+
+
+
+
