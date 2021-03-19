3a3ff0df9e8d481a81e609f710ee626816c94451
Gallium Wang <galliumwang199@gmail.com>
2021-03-19 16:40:50 +0800

    add thought for LC239

---
+++ LeetCode/src/LC239T.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LC239T {
+
+	public LC239T() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+
+/**
+ * 
+ * in every state of the sub array,find the index of peak,
+ * when subarray slide over that peak,find the next peak
+ *
+ */
+
+class Solution239 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+    	
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
