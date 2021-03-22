48fcfb6c3a23e7915915b8f3334dec519ebba0d2
Gallium Wang <galliumwang199@gmail.com>
2021-03-22 12:58:31 +0800

    add thought for LC241

---
+++ LeetCode/src/LC241.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class LC241 {
+
+	public LC241() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/**
+ * 
+ * 	   give different priority to every operator in string and convert it to reverse Polish notation
+ * and then calculate it
+ *
+ */
+
+class Solution241 {
+    public List<Integer> diffWaysToCompute(String expression) {
+        
+    	return null;
+    }
+}
