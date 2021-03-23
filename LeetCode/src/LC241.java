34cd0c7a1b3d439c47eded0b0f7eed27894d8602
Gallium Wang <galliumwang199@gmail.com>
2021-03-23 14:49:40 +0800

    tmp save for uncompleted LC241

--- LeetCode/src/LC241.java
+++ LeetCode/src/LC241.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class LC241 {
 
@@ -21,8 +23,133 @@ public class LC241 {
  */
 
 class Solution241 {
+	private List<List<Integer>> getPermutation(int n) {
+		List<Integer> list=new ArrayList<>();
+		for(int i=0;i<n;i++) {
+			list.add(i);
+		}
+		
+		var res=listPermutations(list);
+		return res;
+	}
+	
+	private List<String> str_to_element_list(String str) {
+		List<String> res=new ArrayList<String>();
+		res.add(Character.toString(str.charAt(0)));
+		
+		for(int i=1;i<str.length();i++) {
+			char currenChr=str.charAt(i);
+			String lastStr=res.get(res.size()-1);
+			if(Character.isDigit(lastStr.charAt(lastStr.length()))&&Character.isDigit(currenChr)) {
+				res.set(res.size()-1,lastStr+Character.toString(currenChr));
+			}
+			else {
+				res.add(Character.toString(currenChr));
+			}
+		}
+		
+		return res;
+	}
+	
+	
+	private int calResult(List<String> exp_list,List<Integer> permutation) {
+		Stack<Integer> priority_stk=new Stack<>();
+		Stack<String> opt_stk=new Stack<>();
+		
+		Stack<String> reverse_polish_stk=new Stack<>();
+		
+		for(String element:exp_list) {
+			if(Character.isDigit(element.charAt(0))) {
+				reverse_polish_stk.push(element);
+			}
+			else {
+				int current_pri=permutation.get(0);
+				while(!opt_stk.empty()&&priority_stk.peek()<current_pri) {
+					priority_stk.pop();
+					reverse_polish_stk.push(opt_stk.pop());
+				}
+				opt_stk.push(element);
+				permutation.remove(0);
+				priority_stk.push(current_pri);
+			}
+		}
+		
+		while(!opt_stk.empty()) {
+			reverse_polish_stk.push(opt_stk.pop());
+		}
+		
+		/**
+		 * calculate reverse polan expression
+		 */
+		
+		// TODO
+		
+		
+		return null;
+	}
+	
     public List<Integer> diffWaysToCompute(String expression) {
+    	List<String> exp_list=str_to_element_list(expression);
+    	
+    	if(expression.length()%2==0) {
+    		System.out.println("wrong input string");
+    		return null;
+    	}
         
-    	return null;
+    	int op_nums=(expression.length()-1)/2;
+    	var permutaions=getPermutation(op_nums);
+    	List<Integer> res=new ArrayList<>();
+    	
+    	for(var per:permutaions) {
+    		int tmpInt=calResult(exp_list, per);
+    		res.add(tmpInt);
+    	}
+    	
+    	return res;
     }
+    
+    public static List<List<Integer>> listPermutations(List<Integer> list) {
+
+        if (list.size() == 0) {
+            List<List<Integer>> result = new ArrayList<List<Integer>>();
+            result.add(new ArrayList<Integer>());
+            return result;
+        }
+
+        List<List<Integer>> returnMe = new ArrayList<List<Integer>>();
+
+        Integer firstElement = list.remove(0);
+
+        List<List<Integer>> recursiveReturn = listPermutations(list);
+        for (List<Integer> li : recursiveReturn) {
+
+            for (int index = 0; index <= li.size(); index++) {
+                List<Integer> temp = new ArrayList<Integer>(li);
+                temp.add(index, firstElement);
+                returnMe.add(temp);
+            }
+
+        }
+        return returnMe;
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
