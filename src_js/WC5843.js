e5094e6689ca3c91144bf61b49e8c5eaa0f23e14
Gallium Wang <galliumwang199@gmail.com>
2021-08-15 18:33:13 +0800

    add accepted solution for wc5843

--- src_js/WC5843.js
+++ src_js/WC5843.js
@@ -3,4 +3,10 @@
  * @param {string} word
  * @return {number}
  */
-var numOfStrings = function (patterns, word) {};
+var numOfStrings = function (patterns, word) {
+  let count = 0;
+  patterns.forEach(function (pattern, index, patterns) {
+    if (word.includes(pattern)) count++;
+  });
+  return count;
+};
